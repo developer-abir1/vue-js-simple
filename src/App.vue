895784e182ef8f9db2f9b6855d86864967d6c1b7
developer abir <developerabir40@gmail.com>
2022-06-16 20:55:52 +0600

    vue manupolet

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup> 
+ 
+import Layout from './componentes/layout/layout.vue';
 </script>
+ <template>
+ <Layout>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+ </Layout>
+ 
+ </template>
 
 <style>
 @import './assets/base.css';
